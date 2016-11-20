e350fdef949c243215087f67bdd9d70be513a070
Gibellino <Gibellino@Gibellinos.lan>
2016-11-20 15:12:19 +0000

    Exercicios

--- exe/loja/Artigo.java
+++ exe/loja/Artigo.java
@@ -1,6 +1,25 @@
 package loja;
 
-public class Artigo {
+public class Artigo{
+	
+	private double preco;
+	private String nome;
+	
+	protected double getPreco() {
+		return preco;
+	}
+
+	protected void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	protected String getNome() {
+		return nome;
+	}
+
+	protected void setNome(String nome) {
+		this.nome = nome;
+	}
 
 	public Artigo() {
 		// TODO Auto-generated constructor stub
