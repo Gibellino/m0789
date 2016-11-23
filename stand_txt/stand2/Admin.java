406a3f69cd3fee9db1702dd7ddec6a0f1c3ec435
Gibellino <bbgibellino@gmail.com>
2016-11-23 10:22:58 +0000

    Stand com gravação em ficheiro txt

---
+++ stand_txt/stand2/Admin.java
@@ -0,0 +1,92 @@
+package stand2;
+
+import java.util.Scanner;
+
+public class Admin extends Carro{
+	
+	Scanner read = new Scanner(System.in);
+	
+	Carro[] carro;
+	
+	public Admin() {
+		// TODO Auto-generated constructor stub
+
+	}
+	
+	public void inserir(){
+		int c;
+		
+		System.out.print("Insira quanto carros quer inserir: ");
+		carro = new Carro[read.nextInt()];
+		
+		System.out.println("");
+		
+		for(int i=0;i<carro.length;i++){
+			
+			//Tem que se inicializar o array de objecto
+			carro[i] = new Carro();
+			
+			System.out.print("Insira a marca do carro " + (i+1) + ": ");
+			carro[i].setMarca(read.nextLine());
+			
+			read.nextLine();
+			
+			System.out.print("Insira o modelo do carro " + (i+1) + ": ");
+			carro[i].setModelo(read.nextLine());
+			
+			System.out.print("Insira a cilindrada do carro " + (i+1) + ": ");
+			carro[i].setCc(read.nextInt());
+			
+			System.out.print("Insira a cavalagem do carro " + (i+1) + ": ");
+			carro[i].setCv(read.nextInt());
+			
+			do{
+			
+			System.out.print("Insira o combustível do carro " + (i+1) + " :\n 1 - Diesel;\n 2 - Gasolina;\n 3 - GPL;\n 4 - Elétrico;\n");
+			c = read.nextInt();
+			
+			switch(c){
+			
+			case 1: carro[i].setComb("Diesel"); c = 0; break;
+			case 2: carro[i].setComb("Gasolina"); c = 0; break;
+			case 3: carro[i].setComb("GPL"); c = 0; break;
+			case 4: carro[i].setComb("Elétrico"); c = 0; break;
+			
+			default: c = 1; System.out.println("\nOpção Inválida!");
+			
+			}
+			
+			}while(c == 1);
+			
+			System.out.print("Insira a cor do carro " + (i+1) + ": ");
+			carro[i].setCor(read.nextLine());
+			
+			read.nextLine();
+			
+			System.out.print("Insira o preço do carro " + (i+1) + ": ");
+			carro[i].setPreco(read.nextInt());
+			
+			System.out.println("");
+			
+		}	
+	}
+	
+	public void mostrar(){
+		if (carro == null) {
+			System.out.println("\nNão há carros!");
+		}
+		else{
+			for(int i=0;i<carro.length;i++){
+				System.out.println("\n\tCarro " + (i+1) + "\n");
+				System.out.println("Marca: " + carro[i].getMarca());
+				System.out.println("Modelo: " + carro[i].getModelo());
+				System.out.println("Cilindrada: " + carro[i].getCc());
+				System.out.println("Cavalagem: " + carro[i].getCv());
+				System.out.println("Combustível: " + carro[i].getComb());
+				System.out.println("Cor: " + carro[i].getCor());
+				System.out.println("Preço: " + carro[i].getPreco());
+			}
+		}
+	}
+	
+}
---
+++ stand_txt/stand2/Stand.java
@@ -0,0 +1,39 @@
+package stand2;
+
+import java.util.Scanner;
+
+public class Stand {
+
+	static Scanner read = new Scanner(System.in);
+	
+	static Admin add = new Admin();
+	
+	public Stand() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		int op;
+		
+		do{
+		
+		System.out.println("\tMenu:\n 1 - Inserir Carros;\n 2 - Listar Carros\n 0 - Sair;\nInsira a opção desejada!");
+		op = read.nextInt();
+		
+		System.out.println("");
+		
+		switch(op){
+		
+		case 1: add.inserir(); break;
+		case 2: add.mostrar(); break;
+		case 0: System.out.println("Vai Sair!!!\n");
+		
+		default: System.out.println("Opção Inválida!\n");
+		}
+		
+		System.out.println("");
+		
+		}while(op != 0);
+	}
+}
