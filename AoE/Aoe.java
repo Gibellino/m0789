f468d3ded785f7d910d5eeb96b618488ce74be3f
Gibellino <bbgibellino@gmail.com>
2016-11-25 07:57:29 +0000

    Age of Empire

---
+++ AoE/Aoe.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Aoe {
+
+	Scanner read = new Scanner(System.in);
+	
+	public Aoe() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("\n    Menu:\n 1- Peasant;\n 2- Warrior;\n 3- Wololo;\n");
+		
+		
+		
+	}
+
+}
